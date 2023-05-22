9c6c559635207b9bcf5fc2e3cebb89e45f95ca80
iljasuckovs <133806620+iljasuckovs@users.noreply.github.com>
2023-05-22 19:17:45 +0300

    DXF.py

--- DXF.py
+++ DXF.py
@@ -3,36 +3,34 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import os
+import ezdxf
 
-"""
-# Welcome to Streamlit!
+def calculate_bounding_box(file_path):
+    try:
+        doc = ezdxf.readfile(file_path)
+        modelspace = doc.modelspace()
+        bounding_box = modelspace.boundary()
+        return bounding_box
+    except ezdxf.DXFError as e:
+        st.error("Error reading DXF file: " + str(e))
+        return None
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def main():
+    st.title("DXF Bounding Box")
+    uploaded_file = st.file_uploader("Upload DXF file", type=["dxf"])
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    if uploaded_file is not None:
+        file_path = os.path.join("uploads", uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        st.success("File uploaded successfully.")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+        bounding_box = calculate_bounding_box(file_path)
+        if bounding_box:
+            st.header("Bounding Box")
+            st.write("Min point:", bounding_box.min)
+            st.write("Max point:", bounding_box.max)
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
