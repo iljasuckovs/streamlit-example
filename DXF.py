5b5ffa457c63f70a246b8b61271632b0b3bb8feb
iljasuckovs <133806620+iljasuckovs@users.noreply.github.com>
2023-05-22 19:45:09 +0300

    DXF.py

--- DXF.py
+++ DXF.py
@@ -1,24 +1,3 @@
 import streamlit as st
-import ezdxf
-
-
-def calculate_bounding_box(file):
-    dxf = ezdxf.read(file)
-    modelspace = dxf.modelspace()
-    bbox = modelspace.bbox()
-    return {
-        'min_point': (bbox.min.x, bbox.min.y),
-        'max_point': (bbox.max.x, bbox.max.y)
-    }
-
-def main():
-    st.title('DXF File Upload')
-    uploaded_file = st.file_uploader('Upload a DXF file', type=['dxf'])
-    if uploaded_file:
-        bounding_box = calculate_bounding_box(uploaded_file)
-        st.header('Bounding Box')
-        st.write('Min Point:', bounding_box['min_point'])
-        st.write('Max Point:', bounding_box['max_point'])
-
-if __name__ == '__main__':
-    main()
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
