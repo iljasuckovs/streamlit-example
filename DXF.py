23c6ba1fe82169bfcac03bedcc8d59e65c2a48c5
iljasuckovs <133806620+iljasuckovs@users.noreply.github.com>
2023-05-22 20:04:08 +0300

    DXF.py

--- DXF.py
+++ DXF.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+import ezdxf
 st.write("DXF bounding box Calculator!")
 st.write("Upload DXF file bellow")
 
@@ -23,3 +24,38 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
+##
+
+from shapely.geometry import Polygon
+
+def get_bounding_box_coordinates(filename):
+    doc = ezdxf.readfile(filename)
+    modelspace = doc.modelspace()
+
+    points = []
+    for entity in modelspace:
+        if entity.dxftype() == 'LINE':
+            points.extend(entity.get_points())
+
+    polygon = Polygon(points)
+    bounds = polygon.bounds
+
+    return bounds
+
+def main():
+    st.title("DXF File Bounding Box")
+
+    st.sidebar.title("Upload DXF File")
+    uploaded_file = st.sidebar.file_uploader("Choose a DXF file", type=".dxf")
+
+    if uploaded_file:
+        bounds = get_bounding_box_coordinates(uploaded_file)
+
+        st.header("Bounding Box Coordinates")
+        st.write("Min X:", bounds[0])
+        st.write("Min Y:", bounds[1])
+        st.write("Max X:", bounds[2])
+        st.write("Max Y:", bounds[3])
+
+if __name__ == "__main__":
+    main()
